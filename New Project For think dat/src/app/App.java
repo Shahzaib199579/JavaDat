7d7ea4ebd16373afaf99f6e737cf31e8220b33b0
Shahzaib Khan <shahzaib.khan@pgc.edu.pk>
2020-01-15 15:07:49 +0500

    Chapter 4 complete, your code is not correct.

--- New Project For think dat/src/app/App.java
+++ New Project For think dat/src/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -12,14 +13,27 @@ public class App {
         // -----------------------------------------------------------
 
         // run a few simple tests
-        @SuppressWarnings("rawtypes")
-        MyArrayList<Integer> mal = new MyArrayList<Integer>();
-		mal.add(1);
-		mal.add(2);
-		mal.add(3);
-		System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
+        // @SuppressWarnings("rawtypes")
+        // MyArrayList<Integer> mal = new MyArrayList<Integer>();
+		// mal.add(1);
+		// mal.add(2);
+		// mal.add(3);
+		// System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
 
-		mal.remove(2);
-		System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
+		// mal.remove(2);
+        // System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
+        
+
+        // Chapter 3
+        // run a few simple tests
+        @SuppressWarnings("rawtypes")
+		List<Integer> mll = new MyLinkedList<Integer>();
+		mll.add(1);
+		mll.add(2);
+		mll.add(3);
+		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
+
+		mll.remove(new Integer(2));
+		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
     }
 }
