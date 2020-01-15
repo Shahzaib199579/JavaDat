49c2c70e235753c0435dfaecbbb48639653da13c
Shahzaib Khan <shahzaib.khan@pgc.edu.pk>
2020-01-15 16:49:25 +0500

    Committing at Chapter 6 section 6.4

--- New Project For think dat/src/app/App.java
+++ New Project For think dat/src/app/App.java
@@ -3,6 +3,13 @@ package app;
 import java.util.Arrays;
 import java.util.List;
 
+import org.jsoup.Connection;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // ListClientExample lce = new ListClientExample();
@@ -26,14 +33,25 @@ public class App {
 
         // Chapter 3
         // run a few simple tests
-        @SuppressWarnings("rawtypes")
-		List<Integer> mll = new MyLinkedList<Integer>();
-		mll.add(1);
-		mll.add(2);
-		mll.add(3);
-		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
+        // @SuppressWarnings("rawtypes")
+		// List<Integer> mll = new MyLinkedList<Integer>();
+		// mll.add(1);
+		// mll.add(2);
+		// mll.add(3);
+		// System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
 
-		mll.remove(new Integer(2));
-		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
+		// mll.remove(new Integer(2));
+        // System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
+        
+        // jsoup
+        @SuppressWarnings("rawtypes")
+        String url = "http://en.wikipedia.org/wiki/Java_(programming_language)";
+
+        Connection conn = Jsoup.connect(url);
+        Document doc = conn.get();
+
+        Element content = doc.getElementById("mw-content-text");
+        Elements paragraphs = content.select("p");
+        System.out.println("ok");
     }
 }
